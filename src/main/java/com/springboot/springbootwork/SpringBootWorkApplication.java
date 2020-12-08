5467a4ec40d46d0c5623502eb5d4e86b67256fbb
Higor Pereira <madhigor@hotmail.com>
2020-12-08 18:27:04 -0300

    Adding user entity and resource

--- src/main/java/com/springboot/springbootwork/SpringBootWorkApplication.java
+++ src/main/java/com/springboot/springbootwork/SpringBootWorkApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootWorkApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootWorkApplication.class, args);
+        
     }
 
 }
